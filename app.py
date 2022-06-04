66f1d6dea15eaf5de39e74cf62036ae99b25c4a3
mc13735292967 <mc13735292967@163.com>
2022-06-04 20:30:24 +0800

    commit
    Signed-off-by: mc13735292967 <mc13735292967@163.com>

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+import hashlib
+import hmac
+import subprocess
+from config import *
+
+def verify_hmac_hash(data, signature):
+    mac = hmac.new(bytes(SECRET, 'UTF-8'), msg=data, digestmod=hashlib.sha256)
+    return hmac.compare_digest('sha256=' + mac.hexdigest(), signature)
+
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    if(verify_hmac_hash(request.data,request.headers.get('X-Hub-Signature-256'))):
+        subprocess.Popen(DEPLOY_COMMAND, shell=True)
+        return "success"
+    else:
+        return("auth failed",500)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='11460', debug=True)
---
+++ readme.md
@@ -0,0 +1,11 @@
+一个用于自动化部署预览站的小工具  
+## 安装
+```
+pip3 install -r requirements.txt
+```
+使用前请修改`config.py.example`文件并删除`.example`后缀
+
+## 启动
+```bash
+python3 app.py
+```
